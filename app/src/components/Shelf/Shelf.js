696e17054f2c2d0ac0f413e25570ef239b804426
Scott Serage <xttocsurge51@gmail.com>
2017-09-22 14:03:16 -0600

    Home Component and Shelf Component able to link to different 'pages'.

--- app/src/components/Shelf/Shelf.js
+++ app/src/components/Shelf/Shelf.js
@@ -1,17 +1,39 @@
 import React, {Component} from "react";
+import {Link} from 'react-router-dom';
 import './Shelf.css';
 
 export default class Shelf extends Component{
   render(){
+    let shelf = this.props.match.params.shelf;
     return (
       <div className="home">
         <header>Shelf Header</header>
         <ul>
-          <li>Bin 1</li>
-          <li>Bin 2</li>
-          <li>Bin 3</li>
-          <li>Bin 4</li>
-          <li>Bin 5</li>
+          <li>
+            <Link to={`/bins/${shelf}/1`}>
+              <button>Bin 1</button>
+            </Link>
+          </li>
+          <li>
+            <Link to={`/bins/${shelf}/2`}>
+              <button>Bin 2</button>
+            </Link>
+          </li>
+          <li>
+            <Link to={`/bins/${shelf}/3`}>
+              <button>Bin 3</button>
+            </Link>
+          </li>
+          <li>
+            <Link to={`/bins/${shelf}/4`}>
+              <button>Bin 4</button>
+            </Link>
+          </li>
+          <li>
+            <Link to={`/bins/${shelf}/5`}>
+              <button>Bin 5</button>
+            </Link>
+          </li>
         </ul>
       </div>
     );
